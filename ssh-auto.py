777f253ae1f4f5608a9827d2d717bead787515ad
yatharthgeek <81706493+yatharthgeek@users.noreply.github.com>
2022-01-13 18:40:16 +0530

    Add files via upload

---
+++ ssh-auto.py
@@ -0,0 +1,26 @@
+import os
+
+print("SSH AUTOMATION")
+print("Tool By YATHARTHGEEK")
+
+print("")
+
+print("Hint : Type Y/N to enable/disable SSH Connection")
+print("")
+print("")
+bash= input("[SSH-AUTO]>>  ")
+
+if bash=="y":
+	print("Enabling SSH Connection")
+	os.system("service ssh start")
+	
+
+if bash=="n":
+	print("ThankYou")
+
+if bash=="connect":
+	inf= input("Enter Username : ")
+	ipaddr = input("Enter IP Address : ")
+	iport = input("Enter Port : ")
+	code = "ssh"+" "+inf+"@"+ipaddr+" "+"-p"+" "+iport
+	os.system(code)
